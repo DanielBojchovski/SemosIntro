759a13336f5d3839734a342ea0a78ef1f65c52a7
Your Name <you@example.com>
2024-11-13 14:58:32 +0100

    new endpoints with authorize

--- SemosIntro/Controllers/WeatherForecastController.cs
+++ SemosIntro/Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SemosIntro.Constants;
 
 namespace SemosIntro.Controllers
 {
@@ -15,6 +17,20 @@ namespace SemosIntro.Controllers
         public IActionResult Get()
         {
             return Ok(Summaries);
+        }
+
+        [Authorize(Roles = StaticUserRoles.USER)]
+        [HttpGet("UserGet")]
+        public IActionResult UserGet()
+        {
+            return Ok("User data");
+        }
+
+        [Authorize(Roles = StaticUserRoles.ADMIN)]
+        [HttpGet("AdminGet")]
+        public IActionResult AdminGet()
+        {
+            return Ok("Admin secret data");
         }
 
     }
