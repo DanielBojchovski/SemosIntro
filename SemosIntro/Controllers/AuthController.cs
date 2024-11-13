048a95712e787887d826b9bb1c73dc3e376bd8e8
Your Name <you@example.com>
2024-11-13 14:48:39 +0100

    register user

--- SemosIntro/Controllers/AuthController.cs
+++ SemosIntro/Controllers/AuthController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SemosIntro.Constants;
+using SemosIntro.Models;
 
 namespace SemosIntro.Controllers
 {
@@ -10,10 +11,12 @@ namespace SemosIntro.Controllers
     public class AuthController : ControllerBase
     {
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public AuthController(RoleManager<IdentityRole> roleManager)
+        public AuthController(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         [HttpPost("SeedRoles")]
@@ -31,6 +34,41 @@ namespace SemosIntro.Controllers
             await _roleManager.CreateAsync(new IdentityRole(StaticUserRoles.ADMIN));
 
             return Ok("Role seeding completed");
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(RegisterRequest request)
+        {
+            IdentityUser? user = await _userManager.FindByEmailAsync(request.Email);
+
+            if (user is not null)
+            {
+                return BadRequest("User already exists");
+            }
+
+            IdentityUser newUser = new IdentityUser
+            {
+                UserName = request.UserName,
+                Email = request.Email,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            IdentityResult createUserResult = await _userManager.CreateAsync(newUser, request.Password);
+
+            if (createUserResult.Succeeded is false)
+            {
+                string errorString = "User creation failsed because: ";
+
+                foreach (IdentityError error in createUserResult.Errors)
+                {
+                    errorString += " # " + error.Description;
+                }
+                return BadRequest(errorString);
+            }
+
+            await _userManager.AddToRoleAsync(newUser, StaticUserRoles.USER);
+
+            return Ok("User created");
         }
     }
 }
